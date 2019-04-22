28cd3f542caca2747ae105ad9176c2f715454085
wley3337 <wley3337@gmail.com>
2019-04-22 17:02:38 -0400

    added content for about sub pages except contact

--- src/components/Exploration.js
+++ src/components/Exploration.js
@@ -3,7 +3,22 @@ import React from 'react'
 
 const Exploration = () =>{
     return (
-        <div>Exploration Page</div>
+        <div className="about-text about-bg">
+        <p className="about-title">What Will is Exploring</p>
+        <p>Increased understanding of:</p>
+        <ul>
+            <li>Rspec</li>
+            <li>JavaScript Testing
+                    <ul>
+                        <li>Mocha</li>
+                        <li>Enzime</li>
+                    </ul>
+            </li>
+            <li>3D Modeling (outside of CAD)</li>
+            <li>C#</li>
+            
+        </ul>
+    </div>
     )
 }
 
--- src/components/WhatWillKnows.js
+++ src/components/WhatWillKnows.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const WhatWillKnows = () =>{
+    const whatWillKnows =["JavaScript", "Rails",
+        "Ruby", "MapboxGL","CSS Grid", "Git Workflow", "RESTfull API design", "SQL", "OOP", "VectorWorks"]
     return (
-        <div>What Will Knows</div>
+        <div className="about-text about-bg">
+            <p className="about-title">What Will Knows</p>
+            <ul>
+            <li>React
+                    <ul>
+                        <li>Redux</li>
+                        <li>Thunk</li>
+                        <li>React-Router</li>
+                    </ul>
+            </li>
+                {whatWillKnows.map( knowledge => <li>{knowledge}</li> ) }
+            </ul>
+        </div>
     )
 }
 export default WhatWillKnows
