424277ef7a4c8a2f3b7915a3ded2407f235b4b3e
wley3337 <wley3337@gmail.com>
2019-04-23 19:41:38 -0400

    bootstrap carosel working

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Carousel from 'react-bootstrap/Carousel';
 
 const MainMenu = () =>{
     return(
-        <div className="main-menu">
-        <Link to="/about/in-a-nutshell" className="main-about"><img src={"/img/about-hat.png"} alt="about page" /></Link>
-        <Link to="/projects" className="main-projects"><img src={"/img/tophat.png"} alt="projects page" /></Link>
-        <Link to="/blogs" className="main-blogs"><img src={"/img/reporter-hat-with-card.png"} alt="blog page" /></Link>
+        <div id="main-menu">
 
+            <Carousel interval={null} indicators={false}>
+                <Carousel.Item>
+                    <div className="main-about d-block">
+                        <Link to="/about/in-a-nutshell" ><img id="about-img" src={"/img/about-hat.png"} alt="about page" /></Link>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div className="main-projects d-block">
+
+                    <Link to="/projects" ><img id="projects-img"src={"/img/tophat.png"} alt="projects page" /></Link>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div className="main-blogs d-block">
+                        <Link to="/blogs" >
+                        <img id="blogs-img"src={"/img/reporter-hat-with-card.png"} alt="blog page" />
+                        </Link>
+
+                    </div>
+
+                </Carousel.Item>
+
+            </Carousel>
         </div>
     )
 }
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Link } from 'react-router-dom'
 //components
 import About from './About'
 import Projects from './Projects'
@@ -9,13 +9,19 @@ import MainMenu from '../components/MainMenu';
 class Main extends Component {
   render() {
     return (
-      <div className="main">
+      <div id="main">
         <Switch>
           <Route path='/about' render ={(props)=> <About {...props}/>} />
           <Route path='/projects' component={Projects} />
           <Route path='/blogs' component={Blogs} />
           <Route component={MainMenu}/>
         </Switch>
+          <div id="home-icon-div">
+
+          <Link to="/" id="home-link"/>
+          </div>
+          
+        
       </div>
     );
   }
