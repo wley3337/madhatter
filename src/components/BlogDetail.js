5c5e5e2b03b847f01db631df2dbc31eda6cfc6d8
wley3337 <wley3337@gmail.com>
2019-04-26 08:02:05 -0400

    styling except for main page changes done

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -3,9 +3,12 @@ import React from 'react'
 const BlogDetail = ({title, desc, link}) =>{
     return(
         <div className="text blog-detail">
-            <p className="blog-detail-title">{title}</p>
+            <div className="blog-detail-header-wrapper">
+                <p className="blog-detail-title">{title}</p>
+                <a href={link} target="_blank" rel="noopener noreferrer" className="blog-detail-img"><div className="blogs-icon"></div></a>
+            
+            </div>
             <p className="blog-detail-desc">{desc}</p>
-            <a href={link} target="_blank" rel="noopener noreferrer" className="blog-detail-img"><div className="blogs-icon"></div></a>
         </div>
     )
 }
--- src/containers/Blogs.js
+++ src/containers/Blogs.js
@@ -5,8 +5,11 @@ import { blogsArray } from '../enumerables/blogsArray'
 const Blogs = () => {
     return(
         <div className="text blogs-main">
-            <p className="blogs-main-title">A sample of some of my blog posts. You can find the compleate list hosted on Medium <a href="https://medium.com/@wley3337" alt="medium profile page" target="_blank" rel="noopener noreferrer" >here</a></p>
-            <div>
+            <span>
+                <p className="blogs-main-title">Here you'll find a sample of my blog posts. You can find the compleate list hosted on <a href="https://medium.com/@wley3337" alt="medium profile page" target="_blank" rel="noopener noreferrer" className="blogs-link-text"> Medium</a></p>
+            </span>
+            
+            <div className="blogs-details-wrapper">
                 {blogsArray.map( blog => <BlogDetail key={blog.id} {...blog}/>)}
 
             </div>
