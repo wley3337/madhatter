44cfd2b6d2622cb36b14a6e76e695e63f24e35b8
wley3337 <wley3337@gmail.com>
2019-04-27 21:34:24 -0400

    desktop styling done"

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
+import MediaQuery from 'react-responsive'
 //components
 import About from './About'
 import Projects from './Projects'
@@ -10,15 +11,34 @@ import HomeButton from '../components/HomeButton'
 class Main extends Component {
   render() {
     return (
-      <div id="main">
-        <Route path="/:any" component={HomeButton}/>
-        <Switch>
-          <Route path='/about' render ={(props)=> <About {...props}/>} />
-          <Route path='/projects' component={Projects} />
-          <Route path='/blogs' component={Blogs} />
-          <Route component={MainMenu}/>
-        </Switch>
-      </div>
+     
+      <React.Fragment>
+        <MediaQuery maxWidth={750} >
+          <div id="main">
+              <Route path="/:any" component={HomeButton}/>
+              <Switch>
+                <Route path='/about' render ={(props)=> <About {...props}/>} />
+                <Route path='/projects' component={Projects} />
+                <Route path='/blogs' component={Blogs} />
+                <Route component={MainMenu}/>
+              </Switch>
+          </div>
+        </MediaQuery>
+          
+        <MediaQuery minWidth={751} >
+          <div id="desktop-background">
+                  <Route component={MainMenu}/>
+            <div id="main">
+                <Switch>
+                  <Route path='/about' render ={(props)=> <About {...props}/>} />
+                  <Route path='/projects' component={Projects} />
+                  <Route path='/blogs' component={Blogs} />
+                </Switch>
+            </div>
+          </div>
+        </MediaQuery>
+      </React.Fragment>
+     
     );
   }
 }
