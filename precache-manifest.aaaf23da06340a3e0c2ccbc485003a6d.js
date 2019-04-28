7fd0a60a72ee739dd7656afc2e243963bdb48d5a
wley3337 <wley3337@gmail.com>
2019-04-27 22:01:03 -0400

    Updates

---
+++ precache-manifest.aaaf23da06340a3e0c2ccbc485003a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a09b51fbdb501d8f5f1",
+    "url": "/madhatter/static/js/runtime~main.99e80f8d.js"
+  },
+  {
+    "revision": "899c9aa9559bd32238a5",
+    "url": "/madhatter/static/js/main.9bd2dcd6.chunk.js"
+  },
+  {
+    "revision": "77501bfdad11d139f42b",
+    "url": "/madhatter/static/js/2.bd16c437.chunk.js"
+  },
+  {
+    "revision": "899c9aa9559bd32238a5",
+    "url": "/madhatter/static/css/main.399d492e.chunk.css"
+  },
+  {
+    "revision": "f782a22a9eb025f28c12e606e20902ef",
+    "url": "/madhatter/index.html"
+  }
+];
